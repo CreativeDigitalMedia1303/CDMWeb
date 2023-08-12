98e5b58f6b52af0b4a44aad6a4c90738996a47e4
CreativeDigitalMedia07 <shashank@creativedigitalmedia.co.in>
2023-08-11 23:24:26 -0700

    Contact

--- src/components/Home/Location.js
+++ src/components/Home/Location.js
@@ -5,7 +5,7 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 const Location = () => {
   const handleLocationClick = () => {
     // Open the location in a new tab
-    window.open('https://maps.app.goo.gl/YQ63Aza41rgZfaov9');
+    window.open('https://www.google.com/maps/place/Maitri+Information+Systems+Pvt.+Ltd,+99,+Jaya+Apartments,+6th+Cross+Rd,+Malleshwara,+Bengaluru,+Karnataka+560003/data=!4m2!3m1!1s0x3bae16267bcefa45:0xd7112d379b898576?utm_source=mstt_1&entry=gps&lucs=47068615&g_ep=CAESCTExLjkwLjMwMhgAINeCAyoINDcwNjg2MTVCAklO');
   };
 
   return (
